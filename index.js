87856712efb13356c9f57062bc3a5fc872f9ffa2
GMalaconi <gmalaconi@protonmail.ch>
2022-01-26 21:22:23 -0300

    Added contetn & dotenv dependency

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
+require('dotenv').config()
 
 app.get("/", function(req, res) {
-    res.send("Hello World");
+    res.sendFile(__dirname + '/view/index.html');
 })
 
 app.listen(8080, () => {
     console.log("Press Ctrl+C to Quit");
+})
+
+app.use('/public', express.static(__dirname + '/public'));
+
+msg = "its not will i am, its actually william."
+
+if (process.env.MESSAGE_STYLE == 'uppercase') {
+    msg = msg.toUpperCase();
+}
+
+app.get("/json", function(req, res) {
+    res.json({
+        "secret": msg
+    })
 })
